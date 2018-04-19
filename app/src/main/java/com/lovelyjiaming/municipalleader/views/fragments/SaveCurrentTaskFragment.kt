82c90329eb9af23eb33b937df900e4b0f7734541
yuanxin <975341137@qq.com>
2018-04-19 16:03:48 +0800

    ui

--- app/src/main/java/com/lovelyjiaming/municipalleader/views/fragments/SaveCurrentTaskFragment.kt
+++ app/src/main/java/com/lovelyjiaming/municipalleader/views/fragments/SaveCurrentTaskFragment.kt
@@ -1,18 +1,19 @@
 package com.lovelyjiaming.municipalleader.views.fragments
 
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import com.lovelyjiaming.municipalleader.R
+import com.lovelyjiaming.municipalleader.views.adapter.CheckNoEndCaseAdapter
+import kotlinx.android.synthetic.main.fragment_save_current_task.*
 
 class SaveCurrentTaskFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,8 +25,13 @@ class SaveCurrentTaskFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_save_current_task, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        save_current_recyclerview.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
+        save_current_recyclerview.adapter = CheckNoEndCaseAdapter(activity as Context)//复用巡查未结案item
+    }
 
     companion object {
-        fun newInstance(param1: String, param2: String) = SaveCurrentTaskFragment()
+        fun newInstance() = SaveCurrentTaskFragment()
     }
 }
--- app/src/main/java/com/lovelyjiaming/municipalleader/views/fragments/SaveFragment.kt
+++ app/src/main/java/com/lovelyjiaming/municipalleader/views/fragments/SaveFragment.kt
@@ -3,13 +3,21 @@ package com.lovelyjiaming.municipalleader.views.fragments
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentStatePagerAdapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import com.lovelyjiaming.municipalleader.R
+import kotlinx.android.synthetic.main.fragment_save.*
 
 class SaveFragment : Fragment() {
+
+    val m_listSaveFragments: List<Fragment> by lazy {
+        listOf(SaveCurrentTaskFragment.newInstance(), SaveWholeCalcuFragment.newInstance())
+    }
+    val m_listSaveFragementsNames: List<String> by lazy {
+        listOf("在施任务", "综合统计")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +29,21 @@ class SaveFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_save, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //
+        viewpager_save.adapter = object : FragmentStatePagerAdapter(childFragmentManager) {
+            override fun getItem(position: Int): Fragment = m_listSaveFragments[position]
+
+            override fun getCount(): Int = 2
+
+            override fun getPageTitle(position: Int): CharSequence? {
+                return m_listSaveFragementsNames[position]
+            }
+        }
+        tbl_save_top.setupWithViewPager(viewpager_save)
+        viewpager_save.currentItem = 0
+    }
 
     companion object {
         fun newInstance() = SaveFragment()
