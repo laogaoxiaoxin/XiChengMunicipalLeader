1f0e75cbbdd003eed4071060a9fda941653158e7
yuanxin <975341137@qq.com>
2018-04-22 18:09:07 +0800

    ui

--- app/src/main/java/com/lovelyjiaming/municipalleader/views/activitys/EngineerUsualDetailActivity.kt
+++ app/src/main/java/com/lovelyjiaming/municipalleader/views/activitys/EngineerUsualDetailActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.lovelyjiaming.municipalleader.R
 import com.lovelyjiaming.municipalleader.utils.AutoUtils
+import kotlinx.android.synthetic.main.activity_engineer_usual_detail.*
 
 class EngineerUsualDetailActivity : AppCompatActivity() {
 
@@ -11,5 +12,7 @@ class EngineerUsualDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_engineer_usual_detail)
         AutoUtils.auto(this)
+
+        engineer_usual_detail_back.setOnClickListener { finish() }
     }
 }
