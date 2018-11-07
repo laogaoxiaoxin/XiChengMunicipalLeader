ebe3d851c5e2cf26ab1d9f976a7aff0c436e3ed8
yuanxin <975341137@qq.com>
2018-11-07 16:15:26 +0800

    road

--- app/src/main/java/com/lovelyjiaming/municipalleader/views/activitys/LoginActivity.kt
+++ app/src/main/java/com/lovelyjiaming/municipalleader/views/activitys/LoginActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Toast
 import com.google.gson.Gson
 import com.lovelyjiaming.municipalleader.R
 import com.lovelyjiaming.municipalleader.utils.AutoUtils
+import com.lovelyjiaming.municipalleader.utils.SpUtils
 import com.lovelyjiaming.municipalleader.utils.XCNetWorkUtil
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -19,6 +20,11 @@ class LoginActivity : AppCompatActivity() {
         AutoUtils.setSize(this, false, 1080, 1920)
         setContentView(R.layout.activity_login)
         AutoUtils.auto(this)
+        if (SpUtils.get(this, "username", "none") != "none") {
+            login_edt_username.setText(SpUtils.get(this, "username", "none").toString())
+            login_edt_password.setText(SpUtils.get(this, "pswd", "none").toString())
+            check_box_remember.isChecked = true
+        }
         //
         cv_login_invoke.setOnClickListener {
             XCNetWorkUtil.invokeGetRequest(this, XCNetWorkUtil.NETWORK_BASIC_CHECK_ADDRESS + "getLogin", {
@@ -26,6 +32,10 @@ class LoginActivity : AppCompatActivity() {
                 if (result.result == "1") {
                     startActivity(Intent(this, MainActivity::class.java))
                     Toast.makeText(this, "登录成功", Toast.LENGTH_LONG).show()
+                    if (check_box_remember.isChecked) {
+                        SpUtils.put(this, "username", login_edt_username.text.toString())
+                        SpUtils.put(this, "pswd", login_edt_password.text.toString())
+                    }
                     finish()
                 }
             }, hashMapOf("phonenumber" to login_edt_username.text.toString(), "password" to login_edt_password.text.toString()))
